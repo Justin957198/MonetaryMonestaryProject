f8208d0c981afd1747841cf53b75323ab14668c8
Justin Johnson <78495404+Justin957198@users.noreply.github.com>
2026-07-02 23:36:50 -0400

    Feat: Adeed a user information page to display user profile information alog with its own js file.

---
+++ inner_website/scripts/userscripts.js
@@ -0,0 +1,56 @@
+window.addEventListener("DOMContentLoaded", loadUser);
+
+
+
+
+async function loadUser() {
+    try {
+        const token = localStorage.getItem('accessToken')
+        const user = await fetch("http://localhost:8080/authenticate/you", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        if(!user.ok || user.status === 401) {
+            //throw new Error("Not Authenticated");
+            refresh();
+            return;
+        }
+
+        frontUser = await user.json();
+        console.log(frontUser)
+        document.getElementById("user-title")
+        .textContent = `User: ${frontUser.username}`;
+        document.getElementById("name-email")
+        .textContent = `Name: ${frontUser.userDetails[0]} -- Email: ${frontUser.email}`
+        document.getElementById("phone")
+        .textContent = `Phone: ${frontUser.userDetails[1]}`
+        document.getElementById("user-title")
+    } catch(ex) {
+        console.log(ex);
+    }
+}
+
+async function refresh() {
+    const newToken = await fetch("http://localhost:8080/authenticate/refresh", {
+        method: "POST",
+        credentials: "include"
+    });
+
+    if(!newToken.ok) {
+        localStorage.removeItem("accessToken");
+        window.location.href = "http://localhost:5500/index.html";
+    }
+
+    const tokenData = await newToken.json();
+    //console.log(tokenData);
+
+    localStorage.setItem(
+        "accessToken",
+        tokenData.token
+    );
+
+    await loadUser();
+}
