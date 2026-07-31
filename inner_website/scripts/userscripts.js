2369e0ac06de9995f1c227e4cde1ba81b165e0d7
Justin Johnson <78495404+Justin957198@users.noreply.github.com>
2026-07-31 00:24:37 -0400

    Feat(user.html): Created empty div for html population to allow for information editing. Feat(userscripts.js): Redid the load user method to only return information and not the whole account. Created a function to generate html for user input for account editing. Created a function that ulilizes fetch to update user information on their account

--- inner_website/scripts/userscripts.js
+++ inner_website/scripts/userscripts.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", loadUser);
+const editInfoBtn = document.getElementById('edit-info')
 
 
 
@@ -6,7 +7,7 @@ window.addEventListener("DOMContentLoaded", loadUser);
 async function loadUser() {
     try {
         const token = localStorage.getItem('accessToken')
-        const user = await fetch("http://localhost:8080/authenticate/you", {
+        const user = await fetch("http://localhost:8080/web/bank/user/userInfo", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -22,13 +23,13 @@ async function loadUser() {
         frontUser = await user.json();
         console.log(frontUser)
         document.getElementById("user-title")
-        .textContent = `User: ${frontUser.username}`;
+        .textContent = `User: ${frontUser[4]}`;
         document.getElementById("name-email")
-        .textContent = `Name: ${frontUser.userDetails[0]} -- Email: ${frontUser.email}`
-        document.getElementById("phone")
-        .textContent = `Phone: ${frontUser.userDetails[1]}`
+        .textContent = `Name: ${frontUser[0]} -- Email: ${frontUser[5]}`;
+        document.getElementById("phone-birthday")
+        .textContent = `Phone: ${frontUser[3]} -- Birthday: ${frontUser[2]}`;
         document.getElementById("address")
-        .textContent = `Current Adress: ${frontUser.userDetails[3]}`
+        .textContent = `Current Adress: ${frontUser[1]}`;
     } catch(ex) {
         console.log(ex);
     }
@@ -55,3 +56,51 @@ async function refresh() {
 
     await loadUser();
 }
+
+function editForm() {
+    document.getElementById('form-output').innerHTML = `
+    <Label>Select The information you wish to edit</label>
+    <select id="option">
+        <option>Name</option>
+        <option>Address</option>
+        <option>Email</option>
+        <option>Phone number</option>
+    </select>
+    <label>New information:</label>
+    <input id="replace-text" type="text">
+    <button id="submit-change" onClick="updateUserInfo()">Submit</button>`
+}
+
+async function updateUserInfo() {
+
+    const option = document.getElementById('option').value;
+    const replacement = document.getElementById('replace-text').value;
+    const editPayload = {
+        option,
+        replacement
+    }
+    try {
+        const token = localStorage.getItem('accessToken')
+        const result = fetch("http://localhost:8080/web/bank/user/manip", {
+            method: "PUT",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(editPayload)
+        })
+
+        if(result == null) {
+            document.getElementById('error-field').innerHTML = `<p>Error in editing profile</p>`
+        }
+
+        const confirmation = (await result).text();
+        document.getElementById('form-output').innerHTML = `<p>Successfully changed account detail ${option} to ${replacement}`
+        await loadUser()
+
+    } catch(ex) {
+        console.log(ex);
+    }
+}
+
+editInfoBtn.addEventListener("click", editForm)
